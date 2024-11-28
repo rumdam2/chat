08a48bc44fe66df9c007f12ca64623a49009c46c
rumdam2 <rumdam2@gmail.com>
2024-11-28 14:56:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -103,6 +103,7 @@ function generateRandomId() {
 function joinChatRoom(postId, post) {
   currentPostId = postId;
   postsListContainer.style.display = "none";
+  createPostContainer.style.display = "none";
   chatContainer.style.display = "block";
   chatWindow.innerHTML = `<h2>Chat for Post: ${post.postId} by ${post.createdBy}</h2>`;
   loadMessages(postId);
